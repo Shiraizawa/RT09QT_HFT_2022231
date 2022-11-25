f4c3f5a3adeb83ba2c53d32fd28005bfa6a20973
Shiraizawa <dorazoltan010416@gmail.com>
2022-11-25 18:05:28 +0100

    CRUD logic added

---
+++ RT09QT_HFT_2022231/RT09QT_HFT_2022231.Logic/CountyLogic.cs
@@ -0,0 +1,42 @@
+﻿using RT09QT_HFT_2022231.Models;
+using RT09QT_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace RT09QT_HFT_2022231.Logic
+{
+    public class CountyLogic
+    {
+        ICountyRepository repository;
+
+        public CountyLogic(ICountyRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(County county)
+        {
+            this.repository.Create(county);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public County Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IEnumerable<County> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(County county)
+        {
+            this.repository.Update(county);
+        }
+    }
+}
---
+++ RT09QT_HFT_2022231/RT09QT_HFT_2022231.Logic/TownLogic.cs
@@ -0,0 +1,45 @@
+﻿using RT09QT_HFT_2022231.Models;
+using RT09QT_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT09QT_HFT_2022231.Logic
+{
+    internal class TownLogic
+    {
+        ITownRepository repository;
+
+        public TownLogic(ITownRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Town town)
+        {
+            this.repository.Create(town);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Town Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IEnumerable<Town> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Town town)
+        {
+            this.repository.Update(town);
+        }
+    }
+}
