4f67d55fbf04b7ef06b507abc828c91c43551f4a
Shiraizawa <dorazoltan010416@gmail.com>
2022-11-26 23:59:49 +0100

    Server vs frontend calls

--- RT09QT_HFT_2022231/RT09QT_HFT_2022231/Program.cs
+++ RT09QT_HFT_2022231/RT09QT_HFT_2022231/Program.cs
@@ -2,34 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using ConsoleTools;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using RT09QT_HFT_2022231.Logic;
+using RT09QT_HFT_2022231.Client;
 using RT09QT_HFT_2022231.Models;
-using RT09QT_HFT_2022231.Repository;
-using RT09QT_HFT_2022231.Test;
+
 
 namespace RT09QT_HFT_2022231
 {
     class Program
     {
-        static CountryLogic countryLogic;
-        static CountyLogic countyLogic;
-        static TownLogic townLogic;
-        static InhabitantLogic inhabitantLogic;
+        static RestService rest;
         static void Main(string[] args)
         {
-            var ctx = new CountryDbContext();
 
-            var countryRepo = new CountryRepository(ctx);
-            var countyRepo= new CountyRepository(ctx);
-            var townRepo= new TownRepository(ctx);
-            var inhabitantRepo= new InhabitantRepository(ctx);
-
-            countryLogic = new CountryLogic(countryRepo);
-            countyLogic=new CountyLogic(countyRepo);
-            townLogic = new TownLogic(townRepo);
-            inhabitantLogic=new InhabitantLogic(inhabitantRepo);
-
+             rest = new RestService("http://localhost:53486", "country");
 
              static void Update(string v)
             {
@@ -38,70 +23,94 @@ namespace RT09QT_HFT_2022231
                     Country item= new Country();
                     Console.Write("Country ID=");
                     item.CountryID=int.Parse(Console.ReadLine());
-                    Console.Write("Country Name=");
+                    Country old = rest.Get<Country>(item.CountryID, "country");
+                    Console.Write($"Country Name [old: {old.CountryName}]=");
                     item.CountryName = Console.ReadLine();
-                    countryLogic.Update(item);
+                    rest.Put(item, "country");
                 }
                 else if (v == "County")
                 {
                     County item = new County();
                     Console.Write("County ID=");
                     item.CountyID = int.Parse(Console.ReadLine());
-                    Console.Write("County Name=");
+                    County old = rest.Get<County>(item.CountyID, "county");
+                    Console.Write($"County Name [old: {old.CountyName}]=");
                     item.CountyName = Console.ReadLine();
-                    Console.Write("Country ID=");
+                    Console.Write($"Country ID [old: {old.CountryID}]=");
                     item.CountryID= int.Parse(Console.ReadLine());
-                    countyLogic.Update(item);
+                    rest.Put(item, "county");
                 }
                 else if (v == "Town")
                 {
                     Town item = new Town();
                     Console.Write("Town ID=");
                     item.TownID = int.Parse(Console.ReadLine());
-                    Console.Write("Town Name=");
+                    Town old = rest.Get<Town>(item.TownID, "town");
+                    Console.Write($"Town Name [old: {old.TownID}]=");
                     item.TownName = Console.ReadLine();
-                    Console.Write("County ID=");
+                    Console.Write($"County ID [old: {old.TownName}]=");
                     item.CountyID= int.Parse(Console.ReadLine());
-                    townLogic.Update(item);
+                    rest.Put(item, "town");
                 }
                 else if (v == "Inhabitant")
                 {
                     Inhabitant item = new Inhabitant();
                     Console.Write("Inhabitant ID=");
                     item.InhabitantID= int.Parse(Console.ReadLine());
-                    Console.Write("Name=");
+                    Inhabitant old = rest.Get<Inhabitant>(item.InhabitantID, "inhabitant");
+                    Console.Write($"Name [old: {old.Name}]= ");
                     item.Name= Console.ReadLine();
-                    Console.Write("Age=");
+                    Console.Write($"Age [old: {old.Age}]=");
                     item.Age = int.Parse(Console.ReadLine());
-                    Console.Write("Sex (true=male/false=female) =");
+                    Console.Write($"Sex (true=male/false=female) [old: {old.Sex} =");
                     item.Sex=bool.Parse(Console.ReadLine());
-                    Console.Write("Location ID=");
+                    Console.Write($"Location ID [old: {old.LocationID}]=");
                     item.LocationID = int.Parse(Console.ReadLine());
-                    inhabitantLogic.Update(item);
+                    rest.Put(item, "inhabitant");
                 }
             }
 
              static void Delete(string v)
             {
-                if (v == "Country") { Console.WriteLine("Format: \n ID"); countryLogic.Delete(int.Parse(Console.ReadLine())); }
-                else if (v == "County") { Console.WriteLine("Format: \n ID"); countyLogic.Delete(int.Parse(Console.ReadLine())); }
-                else if (v == "Town") { Console.WriteLine("Format: \n ID"); townLogic.Delete(int.Parse(Console.ReadLine())); }
-                else if (v == "Inhabitant") { Console.WriteLine("Format: \n ID"); inhabitantLogic.Delete(int.Parse(Console.ReadLine())); }
+                if (v == "Country") 
+                {
+                    List(v);
+                    Console.WriteLine("Format: \n ID");
+                    rest.Delete(int.Parse(Console.ReadLine()),"country");
+                }
+                else if (v == "County")
+                {
+                    List(v);
+                    Console.WriteLine("Format: \n ID");
+                    rest.Delete(int.Parse(Console.ReadLine()), "county");
+                }
+                else if (v == "Town") 
+                {
+                    List(v);
+                    Console.WriteLine("Format: \n ID");
+                    rest.Delete(int.Parse(Console.ReadLine()), "town");
+                }
+                else if (v == "Inhabitant")
+                {
+                    List(v);
+                    Console.WriteLine("Format: \n ID");
+                    rest.Delete(int.Parse(Console.ReadLine()), "inhabitant");
+                }
             }
 
              static void Create(string v)
             {
-                if (v == "Country"){ Console.WriteLine("Format: \n ID#NAME"); Country  item= new Country(Console.ReadLine()); countryLogic.Create(item);  }
-                else if (v == "County") { Console.WriteLine("Format: \n ID#NAME#COUNTRYID"); County item = new County(Console.ReadLine()); countyLogic.Create(item); }
-                else if (v=="Town") { Console.WriteLine("Format: \n ID#NAME#COUNTYID"); Town item = new Town(Console.ReadLine()); townLogic.Create(item); }
-                else if (v=="Inhabitant") { Console.WriteLine("Format: \n ID#NAME#AGE#SEX#LOCATIONID"); Inhabitant item = new Inhabitant(Console.ReadLine()); inhabitantLogic.Create(item); }
+                if (v == "Country"){ Console.WriteLine("Format: \n ID#NAME"); Country  item= new Country(Console.ReadLine()); rest.Post(item,"country");  }
+                else if (v == "County") { Console.WriteLine("Format: \n ID#NAME#COUNTRYID"); County item = new County(Console.ReadLine()); rest.Post(item, "county"); }
+                else if (v=="Town") { Console.WriteLine("Format: \n ID#NAME#COUNTYID"); Town item = new Town(Console.ReadLine()); rest.Post(item, "town"); }
+                else if (v=="Inhabitant") { Console.WriteLine("Format: \n ID#NAME#AGE#SEX#LOCATIONID"); Inhabitant item = new Inhabitant(Console.ReadLine()); rest.Post(item, "inhabitant"); }
             }
 
              static void List(string v)
             {
                 if (v == "Country")
                 {
-                    var items=countryLogic.ReadAll();
+                    List<Country> items = rest.Get<Country>("country");
                     Console.WriteLine("ID" + "\t" + "Name");
                     foreach(var item in items)
                     {
@@ -110,7 +119,7 @@ namespace RT09QT_HFT_2022231
                 }
                 else if(v=="County")
                 {
-                    var items=countyLogic.ReadAll();
+                    List<County> items = rest.Get<County>("county");
                     Console.WriteLine("ID" + "\t" + "Name" +  "\t" +  "CountryID");
                     foreach(var item in items)
                     {
@@ -119,7 +128,7 @@ namespace RT09QT_HFT_2022231
                 }
                 else if (v == "Town")
                 {
-                    var items = townLogic.ReadAll();
+                    List<Town> items = rest.Get<Town>("town");
                     Console.WriteLine("ID" + "\t" + "Name" + "\t" + "CountyID");
                     foreach (var item in items)
                     {
@@ -128,7 +137,7 @@ namespace RT09QT_HFT_2022231
                 }
                 else if (v == "Inhabitant")
                 {
-                    var items = inhabitantLogic.ReadAll();
+                    List<Inhabitant> items = rest.Get<Inhabitant>("inhabitant");
                     Console.WriteLine("ID" + "\t" + "Name" + "\t" +"Age" +"\t" + "Sex" + "\t" + "LocationID");
                     foreach (var item in items)
                     {
