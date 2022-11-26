738c85bedbe6f35864ed13f875e2a527ed85435a
Shiraizawa <dorazoltan010416@gmail.com>
2022-11-26 22:45:11 +0100

    Recreated Endpoint because... issues

---
+++ RT09QT_HFT_2022231/RT09QT_HFT_2022231.Endpoint/Controllers/CountryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RT09QT_HFT_2022231.Logic.Interfaces;
+using System.Collections.Generic;
+
+namespace RT09QT_HFT_2022231.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        ICountryLogic logic;
+
+        public CountryController(ICountryLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        // GET: api/<CountryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CountryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CountryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CountryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CountryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
