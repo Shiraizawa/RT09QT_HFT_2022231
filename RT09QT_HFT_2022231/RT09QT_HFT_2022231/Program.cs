240fef408d127cc9388fe0689a4e57206731b28a
Shiraizawa <dorazoltan010416@gmail.com>
2022-11-26 17:18:45 +0100

    random changes(?)

--- RT09QT_HFT_2022231/RT09QT_HFT_2022231/Program.cs
+++ RT09QT_HFT_2022231/RT09QT_HFT_2022231/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using RT09QT_HFT_2022231.Logic;
 using RT09QT_HFT_2022231.Models;
 using RT09QT_HFT_2022231.Repository;
+using RT09QT_HFT_2022231.Test;
 
 namespace RT09QT_HFT_2022231
 {
@@ -12,6 +15,15 @@ namespace RT09QT_HFT_2022231
             Console.WriteLine("Hello World!");
             CountryDbContext ctx = new CountryDbContext(); ;
             var items = ctx.Countries.ToArray();
+            CountryLogic aaa =new CountryLogic(new CountryRepository(ctx));
+            IEnumerable<int> a= aaa.GetCountyCountPerCountry(0);
+            IEnumerable<int> b = aaa.GetCountyCountPerCountry(1);
+            IEnumerable<int> c = aaa.GetCountyCountPerCountry(2);
+            IEnumerable<int> d = aaa.GetCountyCountPerCountry(3);
+            IEnumerable<int> e = aaa.GetCountyCountPerCountry(4);
+            IEnumerable<CountryInhabitantStatistics> count = aaa.GetInhabitantCountPerCountry();
+            IEnumerable<CountryInhabitantStatistics> count2 = aaa.GetInhabitantCountPerSpecificCountry(1);
+
             ;
         }
     }
